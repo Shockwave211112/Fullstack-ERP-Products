150e146d19f058728c8d92dcd65221193585dfa1
Shockwave211112 <shockwave211112@yandex.ru>
2023-11-01 11:14:27 +0300

    Available local scope

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
@@ -18,4 +19,8 @@ class Product extends Model
         'status'
     ];
 
+    public function scopeAvailable(Builder $query)
+    {
+        return $query->where('status', 'available');
+    }
 }
